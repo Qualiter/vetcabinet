9775268099652d1219d0aacd4e1c8b9bb9656404
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2024-06-30 14:19:35 +0300

    feat: partly added main data

---
+++ src/main/java/com.vetcabinet/cabinet/service/CabinetService.java
@@ -0,0 +1,7 @@
+package com.vetcabinet.cabinet.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CabinetService {
+}
---
+++ src/main/java/com.vetcabinet/clinic/repository/ClinicRepository.java
@@ -0,0 +1,11 @@
+package com.vetcabinet.clinic.repository;
+
+import com.vetcabinet.clinic.model.Clinic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClinicRepository extends JpaRepository<Clinic, UUID> {
+}
